6e900dc6b1aecf656b12446f65a7c41c00ec43e8
Luciana <luciana342010@hotmail.com>
2024-04-19 13:17:20 -0300

    modifiquei a posicao do form e do table

---
+++ main.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form-contatos");
+// criando evento de submit
+form.addEventListener("submit", function (e)) {
+    // remove comportamento de atualizar pagina
+    e.preventDefault();
+}
