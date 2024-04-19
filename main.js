b8f4c44da5d849330d9471d079cc15f511dee7f7
Luciana <luciana342010@hotmail.com>
2024-04-19 15:20:39 -0300

    fazendo acertos no html

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form-contatos");
 // criando evento de submit
-form.addEventListener("submit", function (e)) {
+form.addEventListener("submit", function (e) {
     // remove comportamento de atualizar pagina
     e.preventDefault();
-}
+});
